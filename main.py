5e975436c125d1ce83de113830704d1206f6d54f
Leonardo-Gonzalez-Baldomero <gonzalez.leonardo.vv06@gmail.com>
2026-03-01 15:43:32 -0600

    Segunda parte agregada limpiar texto terminada, falta que lea desde archivos

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-
 def limpiar_valor(valor):
     """
     Limpia un valor individual:
@@ -9,8 +8,7 @@ def limpiar_valor(valor):
     """
     # Tu codigo aqui
     #QUITAR ESPACIOS DEL PRINCIPIO, FINAL Y DE CADA ELEMENTO
-valortotal=''
-valores = ['  1C', ' 2s', '-3    ',  'B   ', '  -2,3', '2.1    ']
+valores = ['  5C', ' 2s', '-3    ',  'B   ', '  -2.3', '2.1    ']
 valores_limpios = [v.strip() for v in valores]  # ['1C', '2s', '-3', 'B', '-2.3', '2.1']
 print(valores_limpios)
 
@@ -25,7 +23,7 @@ def limpiar(texto):
            
 #LIMPIAR LOS VALORES LIMPIOS Y DEJAR SOLO LOS NUMEROS
 datoslimpios = [limpiar(v) for v in valores_limpios]
-print(datoslimpios)
+print(datoslimpios) #['1', '2', '-3', '', '-23', '2.1']
 pass
 
 def procesar_linea(linea):
@@ -38,7 +36,29 @@ def procesar_linea(linea):
     - Retorna el resultado
     """
     # Tu codigo aqui
-    pass
+    #CONVERTIR DE STRING A ENTERO Y LUEGO TRUNCARLOS
+def convertir_a_entero(lista):
+    """Convierte texto a entero, truncando decimales."""
+    if not lista:  # Si esta vacio
+        return 0
+    try:
+        numeros = str(lista)
+        numero = float(numeros)  # Primero a float
+        return int(numero)     # Luego truncar a int
+    except ValueError:
+        return 0  # Si no se puede convertir, es 0
+
+# APLICAR A TODOS LOS ELEMENTOS
+resultados = [convertir_a_entero(t) for t in datoslimpios]
+print(resultados)
+
+#SUMAR LOS VALORES
+def sumar_valores(valores):
+    total = sum(resultados)
+    return total
+
+print("Suma:", sumar_valores(resultados))
+pass
 
 def main():
     """
